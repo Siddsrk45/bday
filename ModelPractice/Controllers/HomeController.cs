5f4751688c6726645f9f9548d25091353ffab5ab
Siddhesh Kumbhar <siddhu.kumbhar2000@gmail.com>
2022-06-20 06:42:09 +0530

    Add project files.

---
+++ ModelPractice/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelPractice.Models;
+
+namespace ModelPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //var data = GetEmployee();
+            Employee emp = new Employee()
+            {
+                Id = 1,
+                Address = "badlapur",
+                Name = "Siddhesh",
+                Dateofbirth = DateTime.Now,
+                isOnline = true
+            };
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Employee emps)
+        {
+            return View();
+        }
+
+
+        public ActionResult About(string name = null)
+        {
+            return View();
+        }
+    
+        //private Employee GetEmployee()
+        //{
+        //    return new Employee()
+        //    {
+        //        Id = 1,
+        //        Address = "India",
+        //        Name = "Siddhesh"
+        //    };
+        //}
+    }
+}
---
+++ ModelPractice/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelPractice.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Display (Name = "Please enter your name")]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool isOnline { get; set; }
+        [Display (Name = " enter your date of birth")] 
+        [DataType (DataType.Date)]
+        public DateTime Dateofbirth  { get; set; }
+      
+
+
+    }
+}
